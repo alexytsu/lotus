fde53bcc9ec81dbd1f48b57bb2d54a4ad89ae9bc
Alex Su <alexanderytsu@gmail.com>
2022-08-26 14:06:35 +1000

    add install helper script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export LIBRARY_PATH=/opt/homebrew/lib
+export FFI_BUILD_FROM_SOURCE=1
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
+export LOTUS_PATH=~/.lotus-local-net
+export LOTUS_MINER_PATH=~/.lotus-miner-local-net
+export LOTUS_SKIP_GENESIS_CHECK=_yes_
+export CGO_CFLAGS_ALLOW="-D__BLST_PORTABLE__"
+export CGO_CFLAGS="-D__BLST_PORTABLE__"
+
+rm -rf ~/.genesis-sectors
+make 2k
+./lotus fetch-params 2048
+./lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
+./lotus-seed genesis new localnet.json
+./lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
